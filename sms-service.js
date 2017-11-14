a93ebff6a825160e0e9acd9557447099796bd340
jkoller90 <jmk4411@gmail.com>
2017-11-14 11:19:53 -0500

    sample templating works

--- sms-service.js
+++ sms-service.js
@@ -80,6 +80,109 @@ app.post('/sms', function (request, response) {
 
 
 // ================================================================
+// Templating for Table
+// ================================================================
+///
+/// Use pug as templating engine. Pug is renamed jade.
+///
+var mysql=require('mysql');
+app.set('view engine', 'pug');
+
+///
+/// HTTP Method	: GET
+/// Endpoint 	: /person
+/// 
+/// To get collection of person saved in MySQL database.
+///
+app.get('/person', function(req, res) {
+	var personList = [];
+
+	// Connect to MySQL database.
+	var connection = 
+    mysql.createConnection({
+	  host     : 'localhost',
+	  user     : 'root',
+	  password : '123456',
+	  database : 'db'
+	});
+	connection.connect();
+
+	// Do the query to get data.
+	connection.query('SELECT * FROM test', function(err, rows, fields) {
+	  	if (err) {
+	  		res.status(500).json({"status_code": 500,"status_message": "internal server error"});
+	  	} else {
+	  		// Loop check on each row
+	  		for (var i = 0; i < rows.length; i++) {
+
+	  			// Create an object to save current row's data
+		  		var person = {
+		  			'name':rows[i].name,
+		  			'address':rows[i].address,
+		  			'phone':rows[i].phone,
+		  			'id':rows[i].id
+		  		}
+		  		// Add object into array
+		  		personList.push(person);
+	  	}
+
+	  	// Render index.pug page using array 
+	  	res.render('index', {"personList": personList});
+	  	}
+	});
+
+	// Close the MySQL connection
+	connection.end();
+	
+});
+
+///
+/// HTTP Method	: GET
+/// Endpoint	: /person/:id
+/// 
+/// To get specific data of person based on their identifier.
+///
+app.get('/person/:id', function(req, res) {
+	// Connect to MySQL database.
+	var connection = 
+    mysql.createConnection({
+	  host     : 'localhost',
+	  user     : 'root',
+	  password : '123456',
+	  database : 'db'
+	});
+
+	// Do the query to get data.
+	connection.query('SELECT * FROM test WHERE id = ' + req.params.id, function(err, rows, fields) {
+		var person;
+
+	  	if (err) {
+	  		res.status(500).json({"status_code": 500,"status_message": "internal server error"});
+	  	} else {
+	  		// Check if the result is found or not
+	  		if(rows.length==1) {
+	  			// Create the object to save the data.
+	  			var person = {
+		  			'name':rows[0].name,
+		  			'address':rows[0].address,
+		  			'phone':rows[0].phone,
+		  			'id':rows[0].id
+		  		}
+		  		// render the details.plug page.
+		  		res.render('details', {"person": person});
+	  		} else {
+	  			// render not found page
+	  			res.status(404).json({"status_code":404, "status_message": "Not found"});
+	  		}
+	  	}
+	});
+
+	// Close MySQL connection
+	connection.end();
+});
+
+
+// ================================================================
 // App Logic
 // ================================================================
 function startLesson() {
@@ -415,10 +518,6 @@ function setHasTakenQuiz(bool, phonenumber) {
 // Get From DB functions
 // ================================================================
 // need to fix
-
-function sqlCon() {
-
-}
 
 function getPhoneNumbers() {
     // TODO Add SQL query
